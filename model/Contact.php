e52c9c88e3a55a50b8d9a645a474ba5f5c4c55d0
Djiyé NGOM <ngomdjiye@gmail.com>
2023-11-06 10:46:22 +0000

    les modification ajouter pour adapter l'utilisateur

--- model/Contact.php
+++ model/Contact.php
@@ -1,5 +1,9 @@
 <?php
 include_once("../config/Database.php");
+if (!empty( $_SESSION['info'])){
+
+    $id_user = $_SESSION['info'][0];}
+
 class Contact {
     private $id;
     private $table_name = "contacts";
@@ -7,12 +11,14 @@ class Contact {
     private $prenom;
     private $numero;
     private $favori;
+    private $id_user;
 
-    public function __construct($prenom,$nom,$numero,$favori) {
+    public function __construct($id_user,$prenom,$nom,$numero,$favori) {
         $this->setPrenom($prenom);
         $this->setNom($nom);
         $this->setNumero($numero);
         $this->setFavori($favori);
+        $this->setId_user($id_user);
     }
 
     // get et setteur 
@@ -44,7 +50,7 @@ class Contact {
         return $this->numero;
     }
     public function setNumero($numero) {
-        if (preg_match("/^(77|78|76)[0-9]{7}/",$numero)) {
+        if (preg_match("/^(77|78|76)\d{7}$/",$numero)) {
         $this->numero = $numero;
     }else{echo "numero  incorrecte" ;}
     }
@@ -58,21 +64,28 @@ class Contact {
             $this->favori = $favori;
         }else {echo'le champ favori est obligatoire';}
     }
+    public function getId_user() {
+        return $this->id_user;
+    }
+    public function setId_user($id_user) {  
+        $this->id_user = $id_user;
+    }
+
 
     
     // Ajouter un contact
     public function ajouterContact($var) {
        
-        $query = "INSERT INTO " . $this->table_name . " (prenom, nom, numero, favori) VALUES (?,?,?,?)";
+        $query = "INSERT INTO " . $this->table_name . " (id_user,prenom, nom, numero, favori) VALUES (?,?,?,?,?)";
         
         $stmt = $var->prepare($query);
-        $stmt->execute(array($this->prenom, $this->nom, $this->numero, $this->favori));
+        $stmt->execute(array($this->id_user,$this->prenom, $this->nom, $this->numero, $this->favori));
        
       }
 
     // Lister tous les contacts
-public static function getAllContacts($var) {
-    $query = $var->query("SELECT id_contact, prenom, nom, numero, favori FROM contacts");
+public static function getAllContacts($var,  $id_user) {
+    $query = $var->query("SELECT id_contact,id_user, prenom, nom, numero, favori FROM contacts WHERE id_user= $id_user ");
     return $query->fetchAll(PDO::FETCH_ASSOC);
 }
 
@@ -85,9 +98,8 @@ public static function getContactById($var, $id) {
     if ($stmt->rowCount() > 0) {
         $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
-        $contact = new Contact($row['prenom'], $row['nom'], $row['numero'], $row['favori']);
-        $contact->setId($row['id_contact']);
-
+        $contact = new Contact($row['id_user'],$row['prenom'], $row['nom'], $row['numero'], $row['favori']);
+         $contact->setId($row['id_contact']);
         return $contact;
     } else {
         return null;
@@ -97,26 +109,25 @@ public static function getContactById($var, $id) {
 public function modifierContact($var) {
     $query = "UPDATE contacts SET prenom = ?, nom = ?, numero = ?, favori = ? WHERE id_contact = ?";
     $stmt = $var->prepare($query);
+        $values = [$this->prenom, $this->nom, $this->numero, $this->favori, $this->id];
 
-    $values = [$this->prenom, $this->nom, $this->numero, $this->favori, $this->id];
-
-    try {
-        $result = $stmt->execute($values);
-
-        if ($result) {
-            echo "La modification a été effectuée avec succès.";
-        } else {
-            echo "La modification a échoué.";
+        try {
+            $result = $stmt->execute($values);
+    
+            if ($result) {
+                echo "La modification a été effectuée avec succès.";
+            } else {
+                echo "La modification a échoué.";
+            }
+        } catch (PDOException $e) {
+            echo "Erreur SQL : " . $e->getMessage();
         }
-    } catch (PDOException $e) {
-        echo "Erreur SQL : " . $e->getMessage();
     }
-}
 //suppression
-public function supprimerContact($var,$id) {
-    $query = "DELETE FROM contacts WHERE id_contact = ?";
-    $stmt = $var->prepare($query);
-    $stmt->execute([$id]);
-}
+            public function supprimerContact($var,$id) {
+                $query = "DELETE FROM contacts WHERE id_contact = ?";
+                $stmt = $var->prepare($query);
+                $stmt->execute([$id]);
+            }
 }
 ?>
